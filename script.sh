2c51fa7c35cc8f43af299c07a5164da5467f2533
EgzoT <przemek7574@windowslive.com>
2023-11-14 22:07:06 +0100

    Update nextjs template. Update node to version 20.

--- script.sh
+++ script.sh
@@ -5,8 +5,7 @@ if [[ -f ./project/package.json ]]; then
     npm install
     npm run dev
 else
-    # npx -y create-react-app my-app --template typescript
-    npx create-next-app@latest my-app --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/typescript-final"
+    npx create-next-app my-app --ts --eslint --tailwind --src-dir --app --import-alias "@/*" --use-npm
 
     cp -r my-app/. project/.
     rm -r my-app
@@ -16,14 +15,15 @@ else
     chown -R node:node /project/public
     chown -R node:node /project/package.json
     chown -R node:node /project/package-lock.json
+    chown -R node:node /project/.gitignore
     chown -R node:node /project/tsconfig.json
-    chown -R node:node /project/components
-    chown -R node:node /project/global.d.ts
-    chown -R node:node /project/lib
+    chown -R node:node /project/src
+    chown -R node:node /project/next.config.js
+    chown -R node:node /project/postcss.config.js
+    chown -R node:node /project/tailwind.config.ts
+    chown -R node:node /project/.eslintrc.json
+    chown -R node:node /project/.next
     chown -R node:node /project/next-env.d.ts
-    chown -R node:node /project/pages
-    chown -R node:node /project/posts
-    chown -R node:node /project/styles
 
     npm install
     npm run dev
